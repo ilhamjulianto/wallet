1151913d0fcb152328270931b638daf04b47b6a9
ilhamst <isembilang@gmail.com>
2019-01-01 16:44:19 +0700

    update dashboard navbar

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -9,21 +9,21 @@ import Transaction from './Transaction'
 import Report from './Report'
 
 export default class index extends Component {
-  componentDidMount = () => {
+  // componentDidMount = () => {
 
-      var lastclear = localStorage.getItem('lastclear'),
-          time_now  = (new Date()).getTime();
+  //     var lastclear = localStorage.getItem('lastclear'),
+  //         time_now  = (new Date()).getTime();
 
-      // .getTime() returns milliseconds so 1000 * 60 * 60 * 24 = 24 days
-      if ((time_now - lastclear) > 1000 * 40) {
+  //     // .getTime() returns milliseconds so 1000 * 60 * 60 * 24 = 24 days
+  //     if ((time_now - lastclear) > 1000 * 40) {
 
-        localStorage.removeItem('token')
+  //       localStorage.removeItem('token')
 
-        localStorage.setItem('lastclear', time_now)
+  //       localStorage.setItem('lastclear', time_now)
         
-        window.location.reload()
-      }
-  }
+  //       window.location.reload()
+  //     }
+  // }
   
   render() {
     if(localStorage.getItem('token') !== null){
